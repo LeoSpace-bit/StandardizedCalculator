3c6e03958db2d956cc4793603eda3ff7e0ecc902
LeoSpace-bit <zihov.leonid@gmail.com>
2022-02-18 00:15:16 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using StandardizedCalculator.NonlinearEquation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StandardizedCalculator
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var equation = new Equation
+            (
+                precision: 5,
+                function: (x, precision) => Math.Round(2 * Math.Sin(x) - 2 - x, precision),
+                firstDerivative: (x, precision) => Math.Round(2 * Math.Cos(x) - 1, precision),
+                secondDerivatives: (x, precision) => Math.Round(-2 * Math.Sin(x), precision)
+            );
+
+            CombinedMethod combinedMethod = new CombinedMethod(equation);
+            var x = combinedMethod.Calculate(-10, 10, Math.Pow(10, -equation.Precision));
+
+            MessageBox.Show($"x = {x} | Ec/ans = {equation.Precision}");
+
+        }
+    }
+}
---
+++ NonlinearEquation/CombinedMethod.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace StandardizedCalculator.NonlinearEquation
+{
+    internal class CombinedMethod
+    {
+        public Equation Equation { get; private set; }
+
+        public CombinedMethod(Equation equation)
+        {
+            Equation = equation;
+        }
+
+        /// <summary>
+        /// Finds the intersection with the x-axis on a range
+        /// </summary>
+        /// <param name="start">Range start value</param>
+        /// <param name="end">Range end value</param>
+        /// <param name="precision">Precision of response (input example: 0.00001)</param>
+        /// <returns>The value that intersects the x-axis at 0 in the specified range</returns>
+        public double Calculate(double start, double end, double precision)
+        {
+            do
+            {
+                if ( Equation.Function(start, Equation.Precision) * Equation.SecondDerivatives(start, Equation.Precision) < 0)
+                {
+                    start -=  Equation.Function(start, Equation.Precision) * (start - end) / (Equation.Function(start, Equation.Precision) - Equation.Function(end, Equation.Precision));
+                }
+                else if ( Equation.Function(start, Equation.Precision) * Equation.SecondDerivatives(start, Equation.Precision) > 0)
+                {
+                    start -= Equation.Function(start, Equation.Precision) / Equation.FirstDerivative(start, Equation.Precision);
+                }
+
+
+                if (Equation.Function(end, Equation.Precision) * Equation.SecondDerivatives(end, Equation.Precision) < 0)
+                {
+                    end -= Equation.Function(end, Equation.Precision) * (end - start) / (Equation.Function(end, Equation.Precision) - Equation.Function(start, Equation.Precision));
+                }
+                else if (Equation.Function(end, Equation.Precision) * Equation.SecondDerivatives(end, Equation.Precision) > 0)
+                {
+                    end -= Equation.Function(end, Equation.Precision) / Equation.FirstDerivative(end, Equation.Precision);
+                }
+
+            } while (Math.Abs(start - end) > 2 * precision);
+
+            return Math.Round((start + end) / 2, Equation.Precision);
+
+        }
+
+    }
+}
