791f33623cea3db1b4da85e248512606adb8406a
LeoSpace-bit <zihov.leonid@gmail.com>
2022-02-21 21:35:43 +0300

    Stable version, up to the ability to calculate your expressions

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -28,55 +29,175 @@ namespace StandardizedCalculator
             InitializeComponent();
         }
 
-        private void Window_Loaded(object sender, RoutedEventArgs e)
+        private void Grid_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e) => DragMove();
+        private void CloseForm_Click(object sender, RoutedEventArgs e) => Close();
+
+        private void NLE_Calculate(object sender, RoutedEventArgs e)
         {
-            //#region SystemOfEquations (1)
+            #region Combine method (3)
 
-            //var mat = new double[4, 5]
-            //{
-            //    { 4.8,  12.5,   -6.3,  -9.7,    3.5     },
-            //    { 22,  -31.7,   12.4,  -8.7,    4.6     },
-            //    { 15,   21.1,   -4.5,   14.4,   15      },
-            //    { 8.6, -14.4,   6.2,    2.8,   -1.2     }
-            //};
+            double value = Math.Abs(CheckPrecision(NLQ_precision, 1));
 
-            //double[] array = new SystemOfEquations.GaussMethod(mat).Calculate();
-            //var values = string.Empty;
-            //for (int i = 0, d = 1; i < array.Length; i++, d++)
-            //{
-            //    values += ($"x{d}: {array[i]}{Environment.NewLine}");
-            //}
-            //MessageBox.Show(values);
+            if (!double.IsNaN(value))
+            {
+                try
+                {
+                    var equation = new Equation
+                    (
+                        precision: (int)Math.Abs(Math.Log10(value)),
+                        function: (x, precision) => Math.Round(2 * Math.Sin(x) - 2 - x, precision),
+                        firstDerivative: (x, precision) => Math.Round(2 * Math.Cos(x) - 1, precision),
+                        secondDerivatives: (x, precision) => Math.Round(-2 * Math.Sin(x), precision)
+                    );
 
-            //#endregion
+                    CombinedMethod combinedMethod = new(equation);
 
-            //#region Integral (2)
+                    double minimum = CheckValue(NLQ_from);
+                    double maximum = CheckValue(NLQ_to);
 
-            ////ans = 0.446 // монте карло --> метод симпсона | 5.6s | 
-            //double f(double x) => (Math.Sqrt(0.4 * x + 1.7)) / (1.5 * x + Math.Sqrt(x * x + 1.3));
+                    NLE_AdditinalText.Visibility = Visibility.Visible;
 
-            //double answer = new Integration.MonteCarloMethod().Calculate(f, 1.2, 2.6, 100000);
-
-            //MessageBox.Show($"{answer}");
-
-            //#endregion
-
-            //#region Combine method (3)
-            //var equation = new Equation
-            //(
-            //    precision: 5,
-            //    function: (x, precision) => Math.Round(2 * Math.Sin(x) - 2 - x, precision),
-            //    firstDerivative: (x, precision) => Math.Round(2 * Math.Cos(x) - 1, precision),
-            //    secondDerivatives: (x, precision) => Math.Round(-2 * Math.Sin(x), precision)
-            //);
-
-            //CombinedMethod combinedMethod = new CombinedMethod(equation);
-            //var x = combinedMethod.Calculate(-10, 10, Math.Pow(10, -equation.Precision));
-
-            //MessageBox.Show($"x = {x} | Ec/ans = {equation.Precision}");
-            //#endregion
+                    NLE_Answer.Content = $"Ответ: {combinedMethod.Calculate(minimum, maximum, Math.Pow(10, -equation.Precision))}";
+                }
+                catch (Exception exc)
+                {
+                    MessageBox.Show($"Ошибка: {exc.Message}");
+                }
+            }
 
 
+            #endregion
         }
+
+        private void Integral_Calculate(object sender, RoutedEventArgs e)
+        {
+            #region Integral (2)
+
+            if(int.TryParse(I_iteration.Text, out int precision))
+            {
+                //ans = 0.446 // монте карло --> метод симпсона |
+                double f(double x) => (Math.Sqrt(0.4 * x + 1.7)) / (1.5 * x + Math.Sqrt(x * x + 1.3));
+
+                if(double.TryParse(I_a.Text.Replace('.', ','), out double i_a))
+                {
+                    if (double.TryParse(I_b.Text.Replace('.', ','), out double i_b))
+                    {
+                        double answer = new Integration.MonteCarloMethod().Calculate(f, i_a, i_b, precision);
+                        I_Answer.Visibility = Visibility.Visible;
+                        I_Answer.Content = $"Ответ: {answer}";
+                    }
+                    else
+                    {
+                        MessageBox.Show("Неверный формат значения 'б'");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Неверный формат значения 'а'");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Неверный формат ввода количества итераций");
+            }
+
+            #endregion
+        }
+
+        private void SoE_Calculate(object sender, RoutedEventArgs e)
+        {
+            #region SystemOfEquations (1)
+
+            double precision = Math.Abs(CheckPrecision(SoE_precision, 1));
+
+            if (!double.IsNaN(precision))
+            {
+                try
+                {
+                    if (double.TryParse(SoE_11.Text.Replace('.', ','), out double x11) && double.TryParse(SoE_12.Text.Replace('.', ','), out double x12) && double.TryParse(SoE_13.Text.Replace('.', ','), out double x13) && double.TryParse(SoE_14.Text.Replace('.', ','), out double x14) && double.TryParse(SoE_1A.Text.Replace('.', ','), out double x1A))
+                    {
+                        if (double.TryParse(SoE_21.Text.Replace('.', ','), out double x21) && double.TryParse(SoE_22.Text.Replace('.', ','), out double x22) && double.TryParse(SoE_23.Text.Replace('.', ','), out double x23) && double.TryParse(SoE_24.Text.Replace('.', ','), out double x24) && double.TryParse(SoE_2A.Text.Replace('.', ','), out double x2A))
+                        {
+                            if (double.TryParse(SoE_31.Text.Replace('.', ','), out double x31) && double.TryParse(SoE_32.Text.Replace('.', ','), out double x32) && double.TryParse(SoE_33.Text.Replace('.', ','), out double x33) && double.TryParse(SoE_34.Text.Replace('.', ','), out double x34) && double.TryParse(SoE_3A.Text.Replace('.', ','), out double x3A))
+                            {
+                                if (double.TryParse(SoE_41.Text.Replace('.', ','), out double x41) && double.TryParse(SoE_42.Text.Replace('.', ','), out double x42) && double.TryParse(SoE_43.Text.Replace('.', ','), out double x43) && double.TryParse(SoE_44.Text.Replace('.', ','), out double x44) && double.TryParse(SoE_4A.Text.Replace('.', ','), out double x4A))
+                                {
+                                    var matrix = new double[4, 5]
+                                    {
+                                        { x11, x12, x13, x14, x1A },
+                                        { x21, x22, x23, x24, x2A },
+                                        { x31, x32, x33, x34, x3A },
+                                        { x41, x42, x43, x44, x4A }
+                                    };
+                                    double[] array = new SystemOfEquations.GaussMethod(matrix).Calculate();
+                                    string values = $"\tx\u2081 = {Math.Round(array[0], (int)Math.Abs(Math.Log10(precision)))}\t\tx\u2082 = {Math.Round(array[1], (int)Math.Abs(Math.Log10(precision)))}{ Environment.NewLine}\tx\u2083 = {Math.Round(array[2], (int)Math.Abs(Math.Log10(precision)))}\t\tx\u2084 = {Math.Round(array[3], (int)Math.Abs(Math.Log10(precision)))}";
+                                    SoE_Answer.Visibility = Visibility.Visible;
+                                    SoE_Answer.Content = $"Ответ: {values}";
+                                }
+                                else
+                                {
+                                    MessageBox.Show("Неверный формат значения в четвёртой строке");
+                                }
+                            }
+                            else
+                            {
+                                MessageBox.Show("Неверный формат значения в третьей строке");
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("Неверный формат значения во второй строке");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Неверный формат значения в первой строке");
+                    }
+                }
+                catch (Exception es)
+                {
+                    Debug.Write(es.Message);
+                }
+            }
+
+            #endregion
+        }
+
+        private double CheckValue(TextBox number)
+        {
+            var text = number.Text.Replace('.', ',');
+            try
+            {
+                return double.Parse(text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Неверный формат");
+                return double.NaN;
+            }
+        }
+
+        private double CheckPrecision(TextBox number, double limit)
+        {
+            var text = number.Text.Replace('.', ',');
+            Regex regex = new Regex(@"^-?[0-9][0-9,\.]+$");
+
+            if (regex.IsMatch(text))
+            {
+                double d = double.Parse(text);
+                if (Math.Abs(d) >= limit)
+                {
+                    MessageBox.Show($"Установлен лимит |x| <= {limit}");
+                    return double.NaN;
+                }
+                return d;
+            }
+            else
+            {
+                MessageBox.Show("Неверный формат");
+            }
+            return double.NaN;
+        }
+
     }
 }
